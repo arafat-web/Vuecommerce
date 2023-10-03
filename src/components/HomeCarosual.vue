341ea5c8489d6e51e92f716baa0b9999db699da0
Arafat Hossain Ar <arafat.122260@gmail.com>
2023-10-03 20:44:17 +0600

    update HomeCarosual.vue

--- src/components/HomeCarosual.vue
+++ src/components/HomeCarosual.vue
@@ -4,24 +4,24 @@
             <div class="carousel-inner">
                 <div class="carousel-item active" data-bs-interval="2000">
                     <img src="https://images.unsplash.com/3/www.madebyvadim.com.jpg?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2082&q=80"
-                        class="d-block w-100" style="height: 80vh;" alt="">
-                    <div class="carousel-caption d-none d-md-block">
+                        class="d-block w-100" style="height: 80vh; object-fit: cover;" alt="">
+                    <div class="carousel-caption">
                         <h5>Men's Collection</h5>
                         <p>Discover the Latest Trends: Explore Our Stylish Men's Collection Today!</p>
                     </div>
                 </div>
                 <div class="carousel-item" data-bs-interval="2000">
                     <img src="https://images.unsplash.com/photo-1567401893414-76b7b1e5a7a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-                        class="d-block w-100" style="height: 80vh;" alt="">
-                    <div class="carousel-caption d-none d-md-block">
+                        class="d-block w-100" style="height: 80vh; object-fit: cover;" alt="">
+                    <div class="carousel-caption">
                         <h5>Women's Collection</h5>
                         <p>Elevate Your Style: Explore Our Exclusive Women's Collection Now!</p>
                     </div>
                 </div>
                 <div class="carousel-item" data-bs-interval="2000">
                     <img src="https://images.unsplash.com/photo-1550009158-9ebf69173e03?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2101&q=80"
-                        class="d-block w-100" style="height: 80vh;" alt="">
-                    <div class="carousel-caption d-none d-md-block">
+                        class="d-block w-100" style="height: 80vh; object-fit: cover;" alt="">
+                    <div class="carousel-caption">
                         <h5>Electronics</h5>
                         <p>Smart Electronics: Explore Tech Marvels for Every Lifestyle!</p>
                     </div>
